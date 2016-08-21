b457b600c513e657570d14f2b23363263336cec7
federicoamura <federicoamura@gmail.com>
2016-08-21 19:35:00 -0300

    Agregados hello worlds y primera prueba de travis

---
+++ AppServer/src/AppServer.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : AppServer.cpp
+// Author      : Taller2
+// Version     :
+// Copyright   : Do not copy
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
