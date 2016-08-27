29a8c2581a81f78de2072d35d23b8944dd90d134
federicoamura <federicoamura@gmail.com>
2016-08-27 17:53:43 -0300

    Cambiado travis a Python y test para verificar

--- Tester/main.py
+++ Tester/main.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
+import unittest
 print "Hello Tester!!!"
+
+def fun(x):
+    return x+1
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(fun(3), 4, "Test Prueba");
