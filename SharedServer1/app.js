d5d78ce07231f0ce2ce10f31a96d2adbf9e1465a
emanuelcondo <emanuelcondo@gmail.com>
2016-09-01 10:45:06 -0300

    limpieza

--- SharedServer1/app.js
+++ SharedServer1/app.js
@@ -29,6 +29,10 @@ if ('development' === app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/test_android', function(req, res){
+	console.log('Android device connected.');
+	res.send('Connected to SharedServer');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,5 +1,5 @@
 # /usr/bin/env sh
 ./AppServer/install-dependencies.sh
 ./ClientApp/install-dependencies.sh
-./SharedServer/install-dependencies.sh
+./SharedServer1/install-dependencies.sh
 ./Tester/install-dependencies.sh
