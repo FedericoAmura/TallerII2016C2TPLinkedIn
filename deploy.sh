149377e7aa5283dc0e55487a39b4d06fb98d8e10
federicoamura <federicoamura@gmail.com>
2016-09-06 16:57:23 -0300

    Prueba get info para commit automatico

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 # /usr/bin/env sh
 #Deployamos a heroku
-
+echo $PWD
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku --version
+git --version
+git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p'
 git subtree push --prefix SharedServer heroku master
