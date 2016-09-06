78aeb08497839ddcb3903dd7e23b13ac64c96af4
federicoamura <federicoamura@gmail.com>
2016-09-06 17:59:13 -0300

    Fix e intento deploy por script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ travis setup heroku
 echo "Versiones:"
 heroku --version
 git --version
-echo "Tratamos de commitear"
+echo "Le metemos el buildpack"
+heroku buildpacks:set heroku/nodejs
 #git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p'
+echo "Y avanzamos con el deploy"
 git subtree push --prefix SharedServer heroku master
