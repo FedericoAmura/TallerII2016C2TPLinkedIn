57b369fb1493b426b48ea167b7bbe1b7ce3727e9
federicoamura <federicoamura@gmail.com>
2016-09-06 17:41:05 -0300

    Prueba deploy por por git

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,8 @@ heroku --version
 git --version
 echo "Tratamos de commitear"
 #git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p'
+eval "$(ssh-agent -s)" #start the ssh agent
+chmod 600 .travis/deploy_key.pem # this key should have push access
+ssh-add .travis/deploy_key.pem
+git remote add deploy https://git.heroku.com/jobifyg2.git
 git subtree push --prefix SharedServer heroku master
