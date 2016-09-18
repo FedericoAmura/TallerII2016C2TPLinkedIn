6e741ba60221648eff7045803f82ed0b82d7b108
federicoamura <federicoamura@gmail.com>
2016-09-18 03:54:15 -0300

    Prueba segun database setup de travis

--- Tester/initiatepostgres.sh
+++ Tester/initiatepostgres.sh
@@ -2,6 +2,6 @@
 echo "----- Generating unix user postgres_user"
 sudo adduser --disabled-password --gecos "" postgres_user
 echo "----- Generating jobify_db database"
-sudo psql -f ./Tester/pgmanagercreate.sql
+sudo psql -U postgres -f ./Tester/pgmanagercreate.sql
 echo "----- Generating jobify_db tables"
 sudo su postgres_user -c "psql -d jobify_db -f ./Tester/jobify_db.sql"
