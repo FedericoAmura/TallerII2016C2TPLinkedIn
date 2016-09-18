7d84961ef15c0b11f273aa41f02041713de98152
federicoamura <federicoamura@gmail.com>
2016-09-18 03:36:30 -0300

    Agregado service postgresql a travis.yml. Revert manual ultimo commit

--- Tester/initiatepostgres.sh
+++ Tester/initiatepostgres.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "----- Generating unix user postgres_user"
-sudo su adduser --disabled-password --gecos "" postgres_user
+sudo adduser --disabled-password --gecos "" postgres_user
 echo "----- Generating jobify_db database"
-sudo su postgres -c "sudo psql -f ./Tester/pgmanagercreate.sql"
+sudo su postgres -c "psql -f ./Tester/pgmanagercreate.sql"
 echo "----- Generating jobify_db tables"
 sudo su postgres_user -c "psql -d jobify_db -f ./Tester/jobify_db.sql"
