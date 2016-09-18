df5cdafa27d99d3923f7116336f40681efd29330
federicoamura <federicoamura@gmail.com>
2016-09-18 03:59:53 -0300

    Script de creacion de tablas segun database setup

--- Tester/initiatepostgres.sh
+++ Tester/initiatepostgres.sh
@@ -4,4 +4,4 @@ sudo adduser --disabled-password --gecos "" postgres_user
 echo "----- Generating jobify_db database"
 sudo psql -U postgres -f ./Tester/pgmanagercreate.sql
 echo "----- Generating jobify_db tables"
-sudo su postgres_user -c "psql -d jobify_db -f ./Tester/jobify_db.sql"
+sudo psql -U postgres_user-d jobify_db -f ./Tester/jobify_db.sql
