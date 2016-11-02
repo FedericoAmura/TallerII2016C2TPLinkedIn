af950ab6cc6ff81b148d5bdc75b607b735de163b
federicoamura <federicoamura@gmail.com>
2016-11-02 00:53:41 -0300

    Agregados iconos para eliminar o editar las categorias. CSS mas lindo y modular

--- SharedServer/public/components.js
+++ SharedServer/public/components.js
@@ -9,14 +9,41 @@ components.directive('categorylist', function() {
   	scope: {
   		category: "="
   	},
-  	controller: function ($scope, $element, $attrs) {
+  	controller: function ($scope, $element, $attrs, $http) {
   		var ctrl = this;
-  		console.log($scope.category);
   		ctrl.category = $scope.category;
+  		
+  		$scope.delete = function() {
+  			$http.delete('/categories/'+escape('outdoor activities')).then(
+  				function (response) {
+  					console.log(response);
+  					console.log(response.data);
+  					console.log('eliminamos outdoor activities');
+  				}, function (rejection) {
+  					console.log(rejection);
+  					console.log(rejection.data);
+  					console.log('no eliminamos nada');
+  				}
+  			);
+  			console.log('frenamos el evento');
+  			var e = window.event;
+			e.cancelBubble = true;
+			if (e.stopPropagation) e.stopPropagation();
+		};
+		
+		$scope.edit = function() {
+			/*
+			aca va el edit de la categoria
+			*/
+  			console.log('frenamos el evento');
+  			var e = window.event;
+			e.cancelBubble = true;
+			if (e.stopPropagation) e.stopPropagation();
+		};
   	},
   	template:
   	'<li>'+
-  		'<div class="collapsible-header">{{category.name}}</div>'+
+  		'<div class="collapsible-header contentdetails"><a class="button btn-flat btn-small" ng-click="delete()"><i class="tiny material-icons">delete</i></a><a class="button btn-flat btn-small" ng-click="edit()"><i class="tiny material-icons">mode_edit</i></a>{{category.name}}</div>'+
   		'<div class="collapsible-body"><p>{{category.description}}</p></div>'+
   	'</li>',
   	replace: true
