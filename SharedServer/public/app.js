12aec8f00080ef5840da4f992d177a414cee15e6
federicoamura <federicoamura@gmail.com>
2016-11-05 18:43:57 -0300

    Fix update, create y delete, todos actualizan estado menos el create (bug)

--- SharedServer/public/app.js
+++ SharedServer/public/app.js
@@ -18,48 +18,28 @@ var jobify = angular.module('jobify', ['components']);
 jobify.controller('CategoriesController', function($scope, $http) {
 	$http.defaults.headers.post["Content-Type"] = "application/json";
 	//conseguimos las categories
-  	var categoriesList = this;
-  	var getCategories = function() {
+  	$scope.getCategories = function() {
   		console.log('actualizamos las categories');
   		$http.get('/categories')
   		.then(function(response) {
-  			categoriesList.categories = response.data.categories;
+  			$scope.categories = response.data.categories;
   		});
   	}
-  	getCategories();
-  	$scope.categories = categoriesList.categories;
+  	$scope.getCategories();
   	
   	$scope.newCategory = function() {
-  		Materialize.toast('Category '+$scope.name+' created', 3000, 'rounded');
   		closeNewCategory();
-  		$scope.name = '';
-  		$scope.description = '';
-  		/*$http({
-  			method:'POST',
-  			url: '/categories',
-  			headers: {'Content-Type': 'application/json'},
-  			data: {'name':'prueba','description':'descripcion'}
-  		}).then(
-  			function (response) {
-				console.log('creamos una category');
-				getCategories();
-			}, function (rejection) {
-				console.log('no creamos nada');
-			}
-  		);*/
-		/*var config = {headers:{'Content-Type':'application/x-www-form-urlencoded'}};
-		var data = $.param({
-		    json: JSON.stringify({category:{name:$scope.name,description:$scope.description}})
-		});*/
-
-  		/*$http.post('/categories',data,config)
-		.success(function (data, status, headers, config) {
-			console.log('todo bien al hacer newCategory')
-			getCategories();
-		})
-		.error(function (data, status, header, config) {
-			console.log('error al hacer newCategory')
-		});*/
+  		data = {category:{name:$scope.categoryname,description:$scope.description}};
+  		$http.post('/categories',data).
+  		then(function (response) {
+  			Materialize.toast('Category '+$scope.categoryname+' created', 3000, 'rounded');
+			$scope.categoryname = '';
+			$scope.description = '';
+  		}, function () {
+			Materialize.toast('Could not create category '+$scope.categoryname, 3000, 'rounded');
+			$scope.categoryname = '';
+			$scope.description = '';
+  		});
 	};
 });
   
--- SharedServer/public/components.js
+++ SharedServer/public/components.js
@@ -10,8 +10,6 @@ components.directive('categorylist', function() {
   		category: "="
   	},
   	controller: function ($scope, $element, $attrs, $http) {
-  		var ctrl = this;
-  		ctrl.category = $scope.category;
   		
   		function stopEventPropagation() {
   			var e = window.event;
@@ -20,42 +18,61 @@ components.directive('categorylist', function() {
   		}
   		
   		$scope.delete = function() {
-  			$http.delete('/categories/'+escape('outdoor activities')).then(
+  			$http.delete('/categories/'+escape($scope.category.name)).then(
   				function (response) {
-  					Materialize.toast(ctrl.category.name+' deleted', 3000, 'rounded');
+  					Materialize.toast($scope.category.name+' deleted', 3000, 'rounded');
+  					$scope.$parent.getCategories();
   				}, function (rejection) {
-  					Materialize.toast('Error deleting '+ctrl.category.name, 3000, 'rounded');
+  					Materialize.toast('Error deleting '+$scope.category.name, 3000, 'rounded');
   				}
   			);
   			stopEventPropagation();
 		};
 		
 		$scope.edit = function() {
-			document.getElementById('editCategoryModal'+ctrl.category.name).style.display = "block";
+			$scope.closeEditCategory();
+			data = {category:{name:$scope.category.newname,description:$scope.category.newdescription}};
+			$http.put('/categories/'+escape($scope.category.name),data).then(
+  				function (response) {
+  					Materialize.toast($scope.category.name+' updated', 3000, 'rounded');
+  					$scope.category.name = $scope.category.newname;
+  					$scope.category.newname = '';
+  					$scope.category.newdescription = '';
+  				}, function (rejection) {
+  					Materialize.toast('Error updating '+$scope.category.name, 3000, 'rounded');
+  					$scope.category.newname = '';
+  					$scope.category.newdescription = '';
+  				}
+  			);
+  			stopEventPropagation();
+		}
+		
+		$scope.showEdit = function() {
+			document.getElementById('editCategoryModal'+$scope.category.name).style.display = "block";
   			stopEventPropagation();
 		};
 
 		$scope.closeEditCategory = function() {
-			document.getElementById('editCategoryModal'+ctrl.category.name).style.display = "none";
+			document.getElementById('editCategoryModal'+$scope.category.name).style.display = "none";
 		}
 	},
   	template:
   	'<li>'+
-  		'<div class="collapsible-header contentdetails"><a class="button btn-flat btn-small" ng-click="delete()"><i class="tiny material-icons">delete</i></a><a class="button btn-flat btn-small" ng-click="edit()"><i class="tiny material-icons">mode_edit</i></a>{{category.name}}</div>'+
+  		'<div class="collapsible-header contentdetails"><a class="button btn-flat btn-small" ng-click="delete()"><i class="tiny material-icons">delete</i></a><a class="button btn-flat btn-small" ng-click="showEdit()"><i class="tiny material-icons">mode_edit</i></a>{{category.name}}</div>'+
   		'<div class="collapsible-body"><p>{{category.description}}</p></div>'+
   		'<!-- modal edit category -->'+
   		'<div id="editCategoryModal{{category.name}}" class="modal-background">'+
   			'<div class="modal-content">'+
 				'<div class="modal-header">'+
 					'<span class="close" ng-click="closeEditCategory()">X</span>'+
-					'<h5>Edit category</h5>'+
+					'<h5>Edit category {{category.name}} </h5>'+
 				'</div>'+
 				'<div class="modal-body">'+
-					'<form method="put" action="/categories">'+
-						'<input type="text" class="active validate" name="name" placeholder="Name" value="{{category.name}}" required><br>'+
-						'<input type="text" class="validate" name="description" placeholder="Description" value="{{category.description}}" required><br>'+
-						'<button type="submit" value="Add" class="btn waves-effect waves-green blue">Edit</button>'+
-					'</form>'+
+					'<div>'+
+						'<input type="text" class="active validate" ng-model="category.newname" placeholder="New name"><br>'+
+						'<input type="text" class="validate" ng-model="category.newdescription" placeholder="New description"><br>'+
+						'<input type="submit" value="Edit" class="btn waves-effect waves-green blue" ng-click="edit()"/>'+
+					'</div>'+
 				'</div>'+
 			'</div>'+
   		'</div>'+
