458e5dc007dde50b1daa9e70344444a60e82ecfa
federicoamura <federicoamura@gmail.com>
2016-11-12 20:06:09 -0300

    Clean dependencies SharedServer

--- SharedServer/install-dependencies.sh
+++ SharedServer/install-dependencies.sh
@@ -1,4 +1,4 @@
 # /usr/bin/env sh
 echo "----- Installing shared server dependencies"
 sudo apt-get install nodejs npm postgresql-9.5
-npm install grunt-cli bower express pg ejs body-parser cookie-parser multer
+npm install express pg body-parser
