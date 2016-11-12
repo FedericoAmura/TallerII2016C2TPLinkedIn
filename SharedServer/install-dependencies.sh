a8c2323e130fba5f8e889ef6d2b04cb724cd0e8c
federicoamura <federicoamura@gmail.com>
2016-11-12 20:19:38 -0300

    Test fix travis npm 2

--- SharedServer/install-dependencies.sh
+++ SharedServer/install-dependencies.sh
@@ -1,6 +1,5 @@
 # /usr/bin/env sh
 echo "----- Installing shared server dependencies"
 sudo apt-get install nodejs npm postgresql-9.5
-npm config rm proxy
-npm config rm https-proxy
+npm config set registry http://registry.npmjs.org
 npm install express pg body-parser
